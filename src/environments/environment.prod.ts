8e23333ec7cc215c1ee573533a100c11f1b5a5d3
Maximus <79494898+Maximus-Raafat@users.noreply.github.com>
2024-03-24 14:04:13 +0200

    disc files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://api.openweathermap.org/data/2.5/weather?',
+  API_KEY: 'ef843dd7e1561a59e0ed984801612c11',
+
 };
